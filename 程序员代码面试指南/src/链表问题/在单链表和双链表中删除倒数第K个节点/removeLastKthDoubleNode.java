680a770919c6a42584cb8597c3dfa5db929e64bb
BdQmq <124289241@qq.com>
2023-09-16 10:46:26 +0800

    由于中文命名包导致的编码错误进行一些编译调整

--- 程序员代码面试指南/src/链表问题/在单链表和双链表中删除倒数第K个节点/removeLastKthDoubleNode.java
+++ 程序员代码面试指南/src/链表问题/在单链表和双链表中删除倒数第K个节点/removeLastKthDoubleNode.java
@@ -6,7 +6,7 @@ import 链表问题.DoubleNode;
  * @author QMQ_bd137
  * @create_time is 2022-03-25 15:31
  */
-public class removeLastKthDoubleDoubleNode<T> {
+class removeLastKthDoubleDoubleNode<T> {
     //通过两次遍历 第一次全链表遍历，每走一步 k值减一。
     //k有三种状态 大于0（没有第K个节点） 等于0（删除头结点） 小于0（删除其他节点）
     public DoubleNode<T> soulution(DoubleNode<T> head, int lastKth) {
